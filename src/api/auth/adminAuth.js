6dcf558872b300daf4e00f5b66c18f643f2926ff
Namrata <namrataawasthi077@gmail.com>
2025-05-18 23:33:38 +0530

    api added

--- src/api/auth/adminAuth.js
+++ src/api/auth/adminAuth.js
@@ -1,7 +1,7 @@
 import { postRequest } from "../requestMethods";
 
 export const login = async (username, password) => {
-    const response = await postRequest(`http://localhost:3000/api/users/login-admin`, {
+    const response = await postRequest(`${import.meta.env.VITE_APP_HOST_URL}/api/users/login-admin`, {
       username: `${username.trim()}`,
       password: `${password.trim()}`,
     });
